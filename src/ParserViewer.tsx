c113614d23226a7587d80e450dd7c62900edcefb
Andrew Gilbert <andrewg800@gmail.com>
2020-12-12 19:24:08 -0800

    Add support for saving recipes

--- src/ParserViewer.tsx
+++ src/ParserViewer.tsx
@@ -6,16 +6,39 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 import { parse } from "./parser";
 import { Order } from "./types";
 
 export default function ParserViewer() {
   const [text, setText] = React.useState("");
+  const [url, setUrl] = React.useState("");
   const recipe = parse(text);
+  function sendToServer() {
+    fetch("http://localhost:5000/example", {
+      method: "PUT",
+      body: JSON.stringify({
+        url: url,
+        example: text,
+        expected: recipe.ingredients
+          .map(
+            (i) =>
+              `${i.amount.quantity ?? ""}|${i.amount.unit ?? ""}|${
+                i.ingredient.name ?? ""
+              }`
+          )
+          .join("\n"),
+      }),
+      headers: { "Content-Type": "application/json;charset=UTF-8" },
+    });
+  }
+
   return (
     <Container>
       <TextareaAutosize onChange={(e) => setText(e.target.value)} />
+      <TextField onChange={(e) => setUrl(e.target.value)} label="Url" />
       <Table>
         <TableHead>
           <TableRow>
@@ -34,6 +57,7 @@ export default function ParserViewer() {
           ))}
         </TableBody>
       </Table>
+      <Button onClick={sendToServer}>Save</Button>
     </Container>
   );
 }
