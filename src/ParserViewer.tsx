abab2b8da2c4abfe347fc83ab5475c2e2c779add
Andrew Gilbert <andrewg800@gmail.com>
2020-12-14 21:51:22 -0800

    Add better validation on ParserViewer
    This makes it easier to add new examples.

--- src/ParserViewer.tsx
+++ src/ParserViewer.tsx
@@ -8,6 +8,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import Alert from "@material-ui/lab/Alert";
 
 import { parse } from "./parser";
 import { Order } from "./types";
@@ -15,8 +16,19 @@ import { Order } from "./types";
 export default function ParserViewer() {
   const [text, setText] = React.useState("");
   const [url, setUrl] = React.useState("");
-  const recipe = parse(text);
+  const [err, setErr] = React.useState<string | null>(null);
+  const [saved, setSaved] = React.useState<boolean>(false);
+  const recipe = parse(text.trim());
+  function reset() {
+    setText("");
+    setUrl("");
+    setErr(null);
+    setSaved(false);
+  }
   function sendToServer() {
+    if (!url) {
+      setErr("Missing url");
+    }
     fetch("http://localhost:5000/example", {
       method: "PUT",
       body: JSON.stringify({
@@ -32,11 +44,29 @@ export default function ParserViewer() {
           .join("\n"),
       }),
       headers: { "Content-Type": "application/json;charset=UTF-8" },
+    }).then((res) => {
+      if (res.ok) {
+        reset();
+        setSaved(true);
+      } else {
+        setErr(text);
+        setSaved(false);
+      }
     });
   }
 
   return (
     <Container>
+      {err && (
+        <Alert elevation={6} variant="filled" severity="error">
+          Error saving example: {err}
+        </Alert>
+      )}
+      {!err && saved && (
+        <Alert elevation={6} variant="filled" severity="success">
+          Example saved.
+        </Alert>
+      )}
       <TextareaAutosize onChange={(e) => setText(e.target.value)} />
       <TextField onChange={(e) => setUrl(e.target.value)} label="Url" />
       <Table>
@@ -48,8 +78,8 @@ export default function ParserViewer() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {recipe.ingredients.map((order: Order) => (
-            <TableRow>
+          {recipe.ingredients.map((order: Order, i: number) => (
+            <TableRow key={order.ingredient.name + i}>
               <TableCell>{order.amount.quantity}</TableCell>
               <TableCell>{order.amount.unit}</TableCell>
               <TableCell>{order.ingredient.name}</TableCell>
@@ -58,6 +88,7 @@ export default function ParserViewer() {
         </TableBody>
       </Table>
       <Button onClick={sendToServer}>Save</Button>
+      <Button onClick={reset}>Reset</Button>
     </Container>
   );
 }
