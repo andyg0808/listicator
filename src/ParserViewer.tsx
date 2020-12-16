ffdd3b22bd01ccd2967b420a4eca387c6ac12a1b
Andrew Gilbert <andrewg800@gmail.com>
2020-12-15 19:27:20 -0800

    Hopefully better error messages for ParserViewer

--- src/ParserViewer.tsx
+++ src/ParserViewer.tsx
@@ -20,7 +20,9 @@ function safeParse(text: string): Recipe {
     console.log(e);
     console.log(JSON.stringify(e, null, "  "));
     const lines = text.trim().split(/\n/);
-    console.log(lines[e.token?.line]);
+    const line = e.token?.line || 0;
+    console.log(line, lines.length);
+    console.log(lines.slice(line - 1, line + 2));
     return { ingredients: [] };
   }
 }
