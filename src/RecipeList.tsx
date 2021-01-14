c980f3f37e5cda7e4d2c4ad73666130e86f8f251
Andrew Gilbert <andrewg800@gmail.com>
2021-01-14 01:21:32 -0800

    Attempt at widget for numerical entry

--- src/RecipeList.tsx
+++ src/RecipeList.tsx
@@ -1,6 +1,9 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
+import Button from "@material-ui/core/Button";
+import InputBase from "@material-ui/core/InputBase";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
@@ -11,6 +14,8 @@ import IconButton from "@material-ui/core/IconButton";
 import TextField from "@material-ui/core/TextField";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
+import Add from "@material-ui/icons/Add";
+import Remove from "@material-ui/icons/Remove";
 
 import { RootState } from "./store";
 import { setMenuSelection } from "./menu_selections";
@@ -78,5 +83,36 @@ export default function RecipeList({ onEdit, onDelete }) {
         );
       })}
     </List>
+  );
+}
+interface CountScrollProps {
+  value: number;
+  label: string;
+  className?: string;
+  onChange: (n: number) => void;
+}
+function CountScroll({ value, label, className, onChange }: CountScrollProps) {
+  return (
+    <InputBase
+      css={{ width: "2em" }}
+      type="number"
+      value={value}
+      onChange={(evt) => onChange(Number(evt.target.value))}
+    />
+  );
+  return (
+    <div css={{}} className={className}>
+      <Button onClick={() => onChange(value + 1)}>
+        <Add />
+      </Button>
+      <InputBase
+        type="number"
+        value={value}
+        onChange={(evt) => onChange(Number(evt.target.value))}
+      />
+      <Button onClick={() => value > 0 && onChange(value - 1)}>
+        <Remove />
+      </Button>
+    </div>
   );
 }
