2d5480cf7056c06c7ff62a29e159ef8e81677a29
Andrew Gilbert <andrewg800@gmail.com>
2021-01-14 01:23:26 -0800

    Clean up `RecipeList` and get nice styling on mobile
    This reconfigures how the buttons are formatted on list items

--- src/RecipeList.tsx
+++ src/RecipeList.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import Button from "@material-ui/core/Button";
 import InputBase from "@material-ui/core/InputBase";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -11,11 +10,8 @@ import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
-import TextField from "@material-ui/core/TextField";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
-import Add from "@material-ui/icons/Add";
-import Remove from "@material-ui/icons/Remove";
 
 import { RootState } from "./store";
 import { setMenuSelection } from "./menu_selections";
@@ -53,7 +49,7 @@ export default function RecipeList({ onEdit, onDelete }) {
       {recipes.map((recipe: Recipe) => {
         const title = recipe.title;
         return (
-          <ListItem key={title}>
+          <ListItem css={{ display: "flex" }} key={title}>
             <ListItemIcon>
               <Checkbox
                 checked={checks[title] || false}
@@ -61,17 +57,25 @@ export default function RecipeList({ onEdit, onDelete }) {
               ></Checkbox>
             </ListItemIcon>
             <ListItemText primary={title}></ListItemText>
-            <ListItemSecondaryAction>
-              {checks[title] && (
-                <TextField
-                  type="number"
-                  label="Quantity"
-                  value={quantities[title] || 1}
-                  onChange={(evt) =>
-                    setQuantity(title, Number(evt.target.value))
-                  }
-                />
-              )}
+            <ListItemSecondaryAction
+              css={{
+                position: "static",
+                transform: "unset",
+                top: "unset",
+                flex: "0 0",
+                display: "flex",
+                padding: "0 16px",
+              }}
+            >
+              <CountScroll
+                css={{
+                  width: "100px",
+                  visibility: checks[title] ? "visible" : "hidden",
+                }}
+                label="Quantity"
+                value={quantities[title] || 1}
+                onChange={(value) => setQuantity(title, value)}
+              />
               <IconButton onClick={() => onEdit(recipe)}>
                 <EditIcon />
               </IconButton>
@@ -99,20 +103,5 @@ function CountScroll({ value, label, className, onChange }: CountScrollProps) {
       value={value}
       onChange={(evt) => onChange(Number(evt.target.value))}
     />
-  );
-  return (
-    <div css={{}} className={className}>
-      <Button onClick={() => onChange(value + 1)}>
-        <Add />
-      </Button>
-      <InputBase
-        type="number"
-        value={value}
-        onChange={(evt) => onChange(Number(evt.target.value))}
-      />
-      <Button onClick={() => value > 0 && onChange(value - 1)}>
-        <Remove />
-      </Button>
-    </div>
   );
 }
