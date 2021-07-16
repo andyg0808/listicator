78911ce0fea2c3522a3b300bd070d37c085590d4
Andrew Gilbert <andrewg800@gmail.com>
2021-07-16 15:19:34 -0700

    Clear menu selections with new list button
    We can use the same Redux action to trigger both things

--- src/menu_selections.ts
+++ src/menu_selections.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import * as R from "ramda";
 
+import { newList } from "./new_list";
 import { MenuSelectionMap } from "./types";
 
 export interface SetMenuSelection {
@@ -16,6 +17,11 @@ const menuSelectionSlice = createSlice({
       const payload = action.payload;
       return R.assoc(payload.name, payload.include, state);
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(newList, (_state, _action: PayloadAction<undefined>) => {
+      return {};
+    });
   },
 });
 
