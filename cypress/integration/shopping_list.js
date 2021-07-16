43dfa3f53b90b5c36f350d9adb2e169f6241f74f
Andrew Gilbert <andrewg800@gmail.com>
2021-07-16 10:21:01 -0700

    Start work on test for ingredient checkboxes

--- cypress/integration/shopping_list.js
+++ cypress/integration/shopping_list.js
@@ -12,10 +12,18 @@ describe("Shopping list", () => {
     cy.visit("http://localhost:3000/");
     markDefaultRecipe();
     addStore("Basic");
+    addStore("Second");
     clickShopTab();
   });
 
   it("should show all the undecided ingredients", () => {
+    cy.contains("h2", "Undecided").next().as("undecided_list");
+    ingredients.forEach((desc) => {
+      cy.get("@undecided_list").contains(desc);
+    });
+  });
+
+  xit("should keep ingredient checkmarks when switching stores", () => {
     cy.contains("h2", "Undecided").next().as("undecided_list");
     ingredients.forEach((desc) => {
       cy.get("@undecided_list").contains(desc);
