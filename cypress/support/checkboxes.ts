c9d3e41e13a89a567a42e07928dfc45c0d7abc50
Andrew Gilbert <andrewg800@gmail.com>
2021-07-16 17:02:36 -0700

    Cypress: Add types to helpers

--- cypress/support/checkboxes.ts
+++ cypress/support/checkboxes.ts
@@ -1,9 +1,9 @@
 const checkbox = 'input[type="checkbox"]';
 
-export function checkItem(item) {
+export function checkItem(item: Cypress.Chainable): Cypress.Chainable {
   return item.find(checkbox).check();
 }
 
-export function uncheckItem(item) {
+export function uncheckItem(item: Cypress.Chainable): Cypress.Chainable {
   return item.find(checkbox).check();
 }
--- cypress/support/lists.ts
+++ cypress/support/lists.ts
@@ -1,13 +1,22 @@
 import { checkItem, uncheckItem } from "./checkboxes";
 
-export function findItem(elem, name: string) {
+export function findItem(
+  elem: Cypress.Chainable,
+  name: string
+): Cypress.Chainable {
   return elem.closest("li");
 }
 
-export function checkListItem(elem, name: string) {
+export function checkListItem(
+  elem: Cypress.Chainable,
+  name: string
+): Cypress.Chainable {
   return checkItem(findItem(elem, name));
 }
 
-export function uncheckListItem(elem, name: string) {
+export function uncheckListItem(
+  elem: Cypress.Chainable,
+  name: string
+): Cypress.Chainable {
   return uncheckItem(findItem(elem, name));
 }
