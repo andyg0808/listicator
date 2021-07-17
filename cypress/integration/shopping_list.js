24259f489716fd73dee8636c0804e52b92f0a07e
Andrew Gilbert <andrewg800@gmail.com>
2021-07-16 17:22:39 -0700

    Add test for checkbox behavior

--- cypress/integration/shopping_list.js
+++ cypress/integration/shopping_list.js
@@ -1,11 +1,20 @@
 import { markDefaultRecipe, ingredients } from "../support/default_recipe";
 import { addStore } from "../support/stores";
+import {
+  checkListItem,
+  listItemShouldBeChecked,
+  listItemShouldNotBeChecked,
+} from "../support/lists";
 
 describe("Shopping list", () => {
   const tab = '[role="tab"]';
 
   function clickShopTab() {
     cy.contains(tab, "Shop").click();
+  }
+
+  function clickBuildTab() {
+    cy.contains(tab, "Build").click();
   }
 
   beforeEach(() => {
@@ -23,10 +32,19 @@ describe("Shopping list", () => {
     });
   });
 
-  xit("should keep ingredient checkmarks when switching stores", () => {
+  it("should keep ingredient checkmarks when switching between tabs", () => {
     cy.contains("h2", "Undecided").next().as("undecided_list");
-    ingredients.forEach((desc) => {
-      cy.get("@undecided_list").contains(desc);
+    const item = Cypress._.sample(ingredients);
+    checkListItem(cy.get("@undecided_list").contains(item));
+    listItemShouldBeChecked(cy.get("@undecided_list").contains(item));
+    clickBuildTab();
+    clickShopTab();
+    cy.contains("h2", "Undecided").next().as("undecided_list");
+    listItemShouldBeChecked(cy.get("@undecided_list").contains(item));
+    ingredients.forEach((i) => {
+      if (i !== item) {
+        listItemShouldNotBeChecked(cy.get("@undecided_list").contains(i));
+      }
     });
   });
 });
