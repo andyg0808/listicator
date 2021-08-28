3c39421537e06fb803866b5e56bd4602dcc27c06
Andrew Gilbert <andrewg800@gmail.com>
2021-08-27 20:25:44 -0700

    First draft toward conversion tab

---
+++ src/ConversionBuilder.tsx
@@ -0,0 +1,66 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import IconButton from "@material-ui/core/IconButton";
+import TextField from "@material-ui/core/TextField";
+
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddIcon from "@material-ui/icons/Add";
+
+import * as R from "ramda";
+
+interface ConversionBuilderProps {
+  onChange: (list: string[]) => void;
+  items: string[];
+}
+
+export function ConversionBuilder({ onChange, items }: ConversionBuilderProps) {
+  const [editing, setEditing] = React.useState("");
+  const addItem = () => {
+    setEditing("");
+    onChange(R.append(editing, items));
+  };
+  return (
+    <List>
+      {items.map((item) => {
+        const dropItem = () => onChange(items.filter((i) => i !== item));
+        return (
+          <ListItem>
+            <ListItemText>{item}</ListItemText>
+            <ListItemSecondaryAction>
+              <IconButton onClick={dropItem} edge="end" aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        );
+      })}
+      <ListItem>
+        <ListItemText>
+          <form
+            onSubmit={(e) => {
+              addItem();
+              e.preventDefault();
+            }}
+          >
+            <TextField
+              label="New store"
+              value={editing}
+              onChange={(e: any) => setEditing(e.target.value)}
+            />
+          </form>
+        </ListItemText>
+        <ListItemSecondaryAction>
+          <IconButton edge="end" aria-label="delete" onClick={addItem}>
+            <AddIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    </List>
+  );
+}
