c26b754dc1e212439f792f90f4d5f5b154c0dfb3
Andrew Gilbert <andrewg800@gmail.com>
2021-08-28 18:05:16 -0700

    Viewer: Extract and support updating ingredients via dropdown
    This required tweaking how we store the text which the Prosemirror
    instance is displaying. It's now stored in the Editor component, in
    order to allow it to be updated by changes to the dropdown values,
    while not getting updated from typing in the editor (we still can't
    afford to use a fully-controlled component, because we don't want to
    try to store all the Prosemirror state in React yet. I might move to a
    different model for wrapping a Prosemirror in React in the future,
    which would probably give me all the things I want for free, more or
    less, but with what I have, this seems like a reasonable system.
    
    I can get away with completely regenerating the Prosemirror on
    dropdown change because the focus should have left the editor, so I
    don't think maintaining the cursor position is as important at that
    point.
    
    This doesn't yet offer _sensible_ alternative ingredients, mind
    you. It just shows that it's possible to update an ingredient via a
    dropdown and have the list of ingredients update appropriately.

---
+++ src/Viewer.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { Order, databaseNumberToString } from "./types";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+
+interface ViewerProps {
+  ingredients: Order[];
+  onUpdate: (current: Order, updated: string) => void;
+}
+export function Viewer({ ingredients, onUpdate }: ViewerProps) {
+  return (
+    <Table data-test="Viewer">
+      <TableHead>
+        <TableRow>
+          <TableCell>Amount</TableCell>
+          <TableCell>Unit</TableCell>
+          <TableCell>Ingredient</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {ingredients.map((order: Order, i: number) => (
+          <TableRow key={order.ingredient.name + i}>
+            <TableCell>
+              {databaseNumberToString(order.amount.quantity)}
+            </TableCell>
+            <TableCell>{order.amount.unit}</TableCell>
+            <TableCell>
+              <IngredientChooser
+                setIngredient={(value: string) => onUpdate(order, value)}
+                current={order.ingredient.name}
+              />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
+
+interface IngredientChooserProps {
+  current: string;
+  setIngredient: (name: string) => void;
+}
+
+function IngredientChooser({ current, setIngredient }: IngredientChooserProps) {
+  const options = [current, "bread"];
+  return (
+    <Select
+      onChange={(e) => setIngredient(String(e.target.value))}
+      value={current}
+    >
+      {options.map((option) => (
+        <MenuItem value={option}>{option}</MenuItem>
+      ))}
+    </Select>
+  );
+}
