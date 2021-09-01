ab8ae1c8b6c3bd21becd59c04e6200009e7e7bb8
Andrew Gilbert <andrewg800@gmail.com>
2021-08-31 22:15:52 -0700

    Show recipe data on conversion tab

--- src/ConversionTab.tsx
+++ src/ConversionTab.tsx
@@ -1,3 +1,27 @@
+import * as React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "./store";
+import { Recipe, Order } from "./types";
+
 export function ConversionTab() {
-  return <p></p>;
+  const recipes = useSelector((state: RootState) => state.recipes.present);
+  const [recipe, setRecipe] = React.useState(recipes[0]);
+
+  return (
+    <div>
+      <div>
+        {recipes.map((recipe: Recipe) => (
+          <p>{recipe.title}</p>
+        ))}
+      </div>
+      <div>
+        <h2>{recipe.title}</h2>
+        <div>
+          {recipe.ingredients.map((order: Order) => (
+            <div>{order.ingredient.name}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
