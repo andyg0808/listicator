7a21c243aef29d7784b2f96bb1e8841576f9bb2f
Andrew Gilbert <andrewg800@gmail.com>
2021-09-02 00:34:46 -0700

    Fix bugged matching
    This wasn't correctly implementing the matching I would have wanted,
    and I've decided I really only want one density entry for each
    product. That way, there's no risk of conflicts.

--- src/conversion_table.ts
+++ src/conversion_table.ts
@@ -1,11 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Density, DensityTable, IngredientId, Unit } from "./types";
 
-interface DensityKey {
-  from: Unit;
-  to: Unit;
-  ingredient: IngredientId;
-}
+type DensityKey = IngredientId;
 
 const conversionTableSlice = createSlice({
   name: "conversionTableSlice",
@@ -14,18 +10,14 @@ const conversionTableSlice = createSlice({
     setConversion(state, action: PayloadAction<Density>) {
       const payload = action.payload;
       return state
-        .filter((d: Density) => !matchDensity(d, payload))
+        .filter((d: Density) => d.ingredient !== payload.ingredient)
         .concat([payload]);
     },
     deleteConversion(state, action: PayloadAction<DensityKey>) {
-      return state.filter((d: Density) => !matchDensity(d, action.payload));
+      return state.filter((d: Density) => d.ingredient !== action.payload);
     },
   },
 });
-
-function matchDensity(left: DensityKey, right: DensityKey) {
-  return left.from === right.from && left.to === right.to;
-}
 
 export const { setConversion, deleteConversion } = conversionTableSlice.actions;
 export const reducer = conversionTableSlice.reducer;
