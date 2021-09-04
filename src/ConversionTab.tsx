74b074233e75fbf10a23843265f5d9dceb8d5d51
Andrew Gilbert <andrewg800@gmail.com>
2021-09-03 21:58:42 -0700

    Better unit conversion entry and switch to turn it on and off
    From a user perspective, it seems like I'll want to enter all the
    conversions, then get rid of the converter inputs. I don't yet see a
    clean way to integrate the converter inputs into the converted data. The
    obvious way would be to fill them in with the original and converted
    values, but we usually want to express our conversion in terms of more
    fundamental quantities (i.e., .25 cup => 32g, rather than 2 cups =>
    256g, since the packages don't typically have the pairs of units we
    want).

--- src/ConversionTab.tsx
+++ src/ConversionTab.tsx
@@ -15,15 +15,18 @@ import {
   StoredFraction,
 } from "./types";
 import Select from "@material-ui/core/Select";
+import Switch from "@material-ui/core/Switch";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 //import TextField from "@material-ui/core/TextField";
-import { TextField } from "formik-material-ui";
+import { TextField, Select as FormikSelect } from "formik-material-ui";
 import Fraction from "fraction.js";
 import { setConversion, deleteConversion } from "./conversion_table";
-import { Form, Formik, Field } from "formik";
+import { Formik, Field } from "formik";
+import { units as lexerUnitTable } from "./lexer";
 import * as yup from "yup";
+import * as R from "ramda";
 
 type ConversionTable = Array<Conversion | Density>;
 
@@ -42,33 +45,52 @@ export function ConversionTab() {
   const recipes = useSelector((state: RootState) => state.recipes.present);
   const densities = useSelector((state: RootState) => state.conversionTable);
   const [recipeId, setRecipe] = React.useState(recipes[0].title);
+  const [showConversions, setShowConversions] = React.useState(true);
   const recipe = recipes.find((r) => r.title === recipeId);
   const target = "gram";
   const conversionTable = ConversionTable.concat(densities);
   const dispatch = useDispatch();
   return (
     <div css={{ maxWidth: "500px", margin: "auto", paddingTop: "30px" }}>
-      <FormControl css={{ width: "100%" }}>
-        <InputLabel>Recipe</InputLabel>
-        <Select
-          value={recipe?.title}
-          onChange={(e) => setRecipe(e.target.value as string)}
-        >
-          {recipes.map((recipe: Recipe, index: number) => (
-            <MenuItem value={recipe.title}>{recipe.title}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      <div css={{ width: "100%", display: "flex" }}>
+        <FormControl>
+          <InputLabel>Recipe</InputLabel>
+          <Select
+            value={recipe?.title}
+            onChange={(e) => setRecipe(e.target.value as string)}
+          >
+            {recipes.map((recipe: Recipe, index: number) => (
+              <MenuItem key={recipe.title} value={recipe.title}>
+                {recipe.title}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl>
+          <InputLabel>Show unit conversion entries</InputLabel>
+          <Switch
+            value={showConversions}
+            onChange={(e) => setShowConversions(e.target.checked)}
+          />
+        </FormControl>
+      </div>
       {recipe && (
         <div>
           <h2>{recipe.title}</h2>
           <div>
-            {recipe.ingredients.map((order: Order) => {
+            {recipe.ingredients.map((order: Order, i: number) => {
               const converted = convertOrder(order, target, conversionTable);
+              // We can't guarantee that there's not duplicate
+              // ingredients, so we don't have a good key value
+              // available. In the future it may be worth the
+              // computation to generate a unique key for each
+              // ingredient name
               return (
-                <div key={order.ingredient.name}>
+                <div>
                   <div>{getDescription(totalOrderFromOrder(converted))}</div>
-                  <ConversionValue order={order} target={target} />
+                  {showConversions && (
+                    <ConversionValue order={order} target={target} />
+                  )}
                 </div>
               );
             })}
@@ -86,6 +108,13 @@ interface ConversionValueProps {
 
 function ConversionValue({ target, order }: ConversionValueProps) {
   const recipes = useSelector((state: RootState) => state.recipes.present);
+  const recipeUnits = recipes.flatMap((r: Recipe) =>
+    r.ingredients
+      .map((o: Order) => o.amount.unit)
+      .filter((u: string | null): u is string => u !== null)
+  );
+  const lexerUnits = lexerUnitTable.map((a: [string, string]) => a[1]);
+  const units = R.uniq(recipeUnits.concat(lexerUnits));
   const densities = useSelector((state: RootState) => state.conversionTable);
   const conversionTable = ConversionTable.concat(densities);
   const dispatch = useDispatch();
@@ -100,43 +129,86 @@ function ConversionValue({ target, order }: ConversionValueProps) {
     return null;
   }
 
-  function updateConversion(value: Fraction | null) {
-    if (value === null) {
-      dispatch(deleteConversion(order.ingredient.name));
-      return;
-    }
+  const currentDensity = densities.find(
+    (d: Density) => d.ingredient === order.ingredient.name
+  );
 
-    dispatch(
-      setConversion({
-        from: source || "",
-        to: target,
-        ingredient: order.ingredient.name,
-        value,
-      })
-    );
-  }
+  // initial values aren't safe, because they could mismatch the from/to/from_unit/to_unit
+  // We need to choose the correct initial value object based on heuristic
   return (
     <Formik
-      initialValues={{ from: factor?.d || "", to: factor?.n || "" }}
-      onSubmit={() => {}}
-      validate={(values) => {
+      initialValues={{
+        from: factor?.d || "",
+        to: factor?.n || "",
+        from_unit: currentDensity?.from || source,
+        to_unit: currentDensity?.to || target || "",
+      }}
+      onSubmit={(values, { setSubmitting }) => {
         if (values.from === "" || values.to === "") {
-          updateConversion(null);
+          dispatch(deleteConversion(order.ingredient.name));
         } else {
-          updateConversion(
-            new Fraction(Number(values.to), Number(values.from))
+          const value = new Fraction(Number(values.to), Number(values.from));
+          dispatch(
+            setConversion({
+              from: values.from_unit,
+              to: values.to_unit,
+              ingredient: order.ingredient.name,
+              value,
+            })
           );
         }
+        setSubmitting(false);
       }}
-      validationSchema={yup
-        .object()
-        .shape({ from: yup.number(), to: yup.number() })}
+      validationSchema={yup.object().shape({
+        from: yup.number(),
+        to: yup.number(),
+        from_unit: yup.string().required(),
+        to_unit: yup.string().required(),
+      })}
     >
       {({ submitForm, isSubmitting }) => (
         <>
-          Conversion: From
-          <Field component={TextField} name="from" label={order.amount.unit} />
-          To <Field component={TextField} name="to" label={target} />
+          Conversion:
+          <Field
+            component={TextField}
+            name="from"
+            label="From"
+            onBlur={() => submitForm()}
+          />
+          <Field
+            component={FormikSelect}
+            name="from_unit"
+            label="Unit to convert from"
+            onBlur={() => submitForm()}
+          >
+            {units.map((unit) => {
+              return (
+                <MenuItem key={unit} value={unit}>
+                  {unit}
+                </MenuItem>
+              );
+            })}
+          </Field>
+          <Field
+            component={TextField}
+            name="to"
+            label="To"
+            onBlur={() => submitForm()}
+          />
+          <Field
+            component={FormikSelect}
+            name="to_unit"
+            label="Unit to convert to"
+            onBlur={() => submitForm()}
+          >
+            {units.map((unit) => {
+              return (
+                <MenuItem key={unit} value={unit}>
+                  {unit}
+                </MenuItem>
+              );
+            })}
+          </Field>
         </>
       )}
     </Formik>
@@ -189,12 +261,27 @@ function addInversions(conversionTable: ConversionTable): ConversionTable {
     .concat(conversionTable);
 }
 
+/** Find conversion from `unit` to `target`. Uses the provided
+conversion table and only selects units which are not specific to an
+ingredient or those which specify `ingredient`. This is used to allow
+density-based conversions to be specified (primarily volume units to
+grams). */
 function shortestPath(
   unit: string,
   target: string,
   ingredient: IngredientId,
   conversionTable: ConversionTable
 ): Fraction | null {
+  /*
+   * Graph-based technique inspired by technique from T. Wildi, Metric
+   * Units and Conversion Charts: A Metrication Handbook for Engineers,
+   * Technologists, and Scientists, 2nd ed., Piscataway, NJ: IEEE Press,
+   * 1995. as described in G. S. Novak, "Conversion of units of
+   * measurement," in IEEE Transactions on Software Engineering,
+   * vol. 21, no. 8, pp. 651-661, Aug. 1995, doi:
+   * 10.1109/32.403789. (https://www.cs.utexas.edu/users/novak/units95.html)
+   */
+
   /** @param {Set<string>} seen A (mutable) set of conversions we've tried. */
   function _shortestPath(unit: string, seen: Set<string>): Fraction | null {
     seen.add(unit);
