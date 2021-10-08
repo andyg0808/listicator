8b5850208437e1b6d955fb894953b7468155b81a
Andrew Gilbert <andrewg800@gmail.com>
2021-10-07 21:22:01 -0700

    Add a couple tests

--- src/unparse.test.ts
+++ src/unparse.test.ts
@@ -2,6 +2,7 @@ import { unparse, parse } from "./parser";
 import fc from "fast-check";
 import { fc_order, normalizeOrder } from "./test_generators";
 import { Order } from "./types";
+import { units } from "./lexer";
 
 declare global {
   namespace jest {
@@ -112,6 +113,20 @@ describe("unparse", () => {
         expect(unparse(orders)).toParseAs(
           expected.length === 0 ? null : expected
         );
+      })
+    );
+  });
+
+  it("handles tricky cases such as headerlike ingredients", () => {
+    expect("!!A:").toRoundTrip();
+    expect(`1 can tomato paste`).toRoundTrip();
+  });
+
+  it("uses a minimal number of exclamation points", () => {
+    const unitNames = units.map((u) => u[1]);
+    fc.assert(
+      fc.property(fc.constantFrom(...unitNames), (unit) => {
+        expect(`1 ${unit} oatmeal`).toRoundTrip();
       })
     );
   });
